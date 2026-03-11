962b3f2436e0a75f5b2a1d28cfe46635ba6b6f30
Chaitanya <chaitanyakondoju07@gmail.com>
2026-03-11 17:29:12 +0530

    Added Unit test using Pytest to CI/CD Pipeline

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-print("This is CI demo using python ")
+def add(a, b):
+    return a + b
 
-a = 10
-b = 20
-print("Sum =", a + b)
+if __name__ == "__main__":
+    result = add(10, 20)
+    print("This is CI demo using python")
+    print("Sum =", result)
